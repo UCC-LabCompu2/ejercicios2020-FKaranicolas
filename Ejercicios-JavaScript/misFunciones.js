a41dab87db3ba97800cfe8b55a6178bb9df4f817
FKaranicolas <facukaranicolas@gmail.com>
2020-05-14 19:09:34 -0300

    Se agregaron ejercios de mostrat_ocultar y OperacionesMatematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,6 +5,13 @@
  * @param {number} valor- El valor de metros, yardas, pies y pulgadas.
  * @return
  */
+/**
+ * Conversion de unidades de grados a radianes y viceversa.
+ * @method convertir GR.
+ * @param  {string} id - EL id de los imputs de grados y radianes.
+ * @return
+ */
+
 
 function cambiarUnidades(id, valor) {
     if (isNaN(valor)) {
@@ -43,4 +50,45 @@ function convertirGR(id) {
     }
     document.getElementById("grados").value = grad;
     document.getElementById("radianes").value = rad;
+}
+
+function mostrar_ocultar(valorMO) {
+
+    if (valorMO=="val_mostrar"){
+        document.getElementById("divMO").style.display = 'block';
+    }else if (valorMO=="val_ocultar"){
+        document.getElementById("divMO").style.display = 'none';
+    }
+}
+
+function calcularSuma() {
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value= num1 + Number(num2);
+}
+
+function calcularResta() {
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("res_num1")[0].value);
+    num2=document.getElementsByName("res_num2")[0].value;
+    document.getElementsByName("res_total")[0].value= num1 - Number(num2);
+}
+
+function calcularMultiplicacion() {
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("mul_num1")[0].value);
+    num2=document.getElementsByName("mul_num2")[0].value;
+    document.getElementsByName("mul_total")[0].value= num1 * Number(num2);
+}
+
+function calcularDivision() {
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("div_num1")[0].value);
+    num2=document.getElementsByName("div_num2")[0].value;
+    document.getElementsByName("div_total")[0].value= num1 / Number(num2);
 }
